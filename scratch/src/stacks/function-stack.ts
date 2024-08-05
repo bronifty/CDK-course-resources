78cf63841ee6feab025cdb82f3656753ef0b609b
bronifty <bronifty@gmail.com>
2024-08-04 20:46:11 -0400

    this

--- scratch/src/stacks/function-stack.ts
+++ scratch/src/stacks/function-stack.ts
@@ -6,12 +6,29 @@ class FunctionStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: cdk.StackProps) {
     super(scope, id, props);
 
-    new cdk.aws_lambda.Function(this, "HelloWorldFunction", {
-      runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
-      handler: "lambda.handler",
-      code: cdk.aws_lambda.Code.fromAsset(
-        `${fsUtils.getProjectRoot()}/src/services/hello`
-      ),
+    const helloWorldFunction = new cdk.aws_lambda.Function(
+      this,
+      "HelloWorldFunction",
+      {
+        runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+        handler: "lambda.handler",
+        code: cdk.aws_lambda.Code.fromAsset(
+          `${fsUtils.getProjectRoot()}/dist/services/hello`
+        ),
+      }
+    );
+
+    const helloWorldFunctionUrl = new cdk.aws_lambda.FunctionUrl(
+      this,
+      "HelloWorldFunctionUrl",
+      {
+        function: helloWorldFunction,
+        authType: cdk.aws_lambda.FunctionUrlAuthType.NONE,
+      }
+    );
+
+    new cdk.CfnOutput(this, "FunctionUrl", {
+      value: helloWorldFunctionUrl.url.toString(),
     });
   }
 }
